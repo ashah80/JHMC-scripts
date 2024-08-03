36db20d52bd5f0be829c91f7d4315f2375794e2f
skyelarreuter <sreuter@imsa.edu>
2024-08-03 15:56:24 -0500

    added 2 pages

---
+++ script.js
@@ -0,0 +1,27 @@
+//this if for the collapsible btns that will wants
+const collapsibleBtns = document.querySelectorAll('.collapsible-btn');
+const collapsibleContents = document.querySelectorAll('.collapsible-content');
+
+collapsibleBtns.forEach((btn, index) => {
+    btn.addEventListener('click', function() {
+        collapsibleContents[index].classList.toggle('collapsed');
+    });
+});
+
+//IGNORE THIS PART - THIS IS FOR AARAV
+const sidebar = document.querySelector('.sidebar');
+const toggleButton = document.querySelector('.toggle-sidebar');
+const caret = toggleButton.querySelector('.fa-angle-right');
+
+
+toggleButton.addEventListener('click', () => {
+    sidebar.style.transform = sidebar.style.transform === 'translateX(-250px)' ? '' : 'translateX(-250px)';
+    caret.style.transform = caret.style.transform === 'rotate(180deg)' ? '' : 'rotate(180deg)';
+});
+
+const menuToggle = document.querySelector('.toggle-sidebar');
+const wrapper = document.querySelector('.wrapper');
+
+menuToggle.addEventListener('click', () => {
+    wrapper.classList.toggle('menu-open');
+});
